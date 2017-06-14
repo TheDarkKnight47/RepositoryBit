90afa42217cedca08a6de7060a32175fa5bafd05
Ajay Sharma <ajay.sharma@finicity.com>
2017-06-14 11:49:24 +0530

    - First branch

---
+++ src/main/java/com/mycompany/jsonbtest/FirstClass.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.jsonbtest;
+
+/**
+ *
+ * @author ahsharma
+ */
+public class FirstClass {
+
+    @Override
+    public String toString() {
+    
+        String str = super.toString();
+        return "First Class Created at "+str;
+    }
+    
+}
