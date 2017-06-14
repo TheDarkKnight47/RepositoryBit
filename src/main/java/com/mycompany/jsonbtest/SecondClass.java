eaec49b89816022d2adafe97786e3e6c05f22516
Ajay Sharma <ajay.sharma@finicity.com>
2017-06-14 11:51:13 +0530

    - Second branch

---
+++ src/main/java/com/mycompany/jsonbtest/SecondClass.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.jsonbtest;
+
+/**
+ *
+ * @author ahsharma
+ */
+public class SecondClass {
+
+    @Override
+    public String toString() {
+        String str =  super.toString(); //To change body of generated methods, choose Tools | Templates.
+        return "Second Class at "+str;
+    }
+    
+    
+}
